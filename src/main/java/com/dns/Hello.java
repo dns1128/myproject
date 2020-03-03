3f9db57a053d98e91253b00f486364fc90692fc6
dns1128 <b8716030@gmail.com>
2020-03-04 00:01:52 +0800

    Java and Kotilin variable,data type

--- src/main/java/com/dns/Hello.java
+++ src/main/java/com/dns/Hello.java
@@ -5,5 +5,14 @@ public class Hello {
         //System.out.println("Hello world");
         Person p = new Person();
         p.hello();
+        int age = 10;
+        char c = '我';
+        byte b = 120;
+        float weight = 40.5f;
+        boolean aduit = false;
+        String name ="kage";
+        //String為參照資料型態
+        Integer age2 = 19;
+
     }
 }
--- src/main/kotlin/com/dns/Hello.kt
+++ src/main/kotlin/com/dns/Hello.kt
@@ -6,6 +6,11 @@ fun main() {
     val h = Human()
     //val 定義完後不能再更改
     h.hello()
+    var age :Int = 19;
+    age = 20;
+    var weight = 22.5f;
+    var name : String;
+    name = "hank";
 }
 
 class Human {
